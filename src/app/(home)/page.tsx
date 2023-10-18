9f5d2834b4c15d5c328c41078546343c919c7ac6
Jhony Pereira <jhony-16@live.com>
2023-10-18 12:48:56 -0300

    FEAT: add ProdcutItem and ProductList components

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,22 +1,33 @@
-"use client";
-
 import Image from "next/image";
 import Categories from "./components/catergories";
+import { prismaClient } from "@/lib/prisma";
+import ProductList from "./components/product-horizontal-list";
 
-export default function Home() {
+export default async function Home() {
+  const deals = await prismaClient.product.findMany({
+    where: {
+      discountPercentage: {
+        gt: 0,
+      },
+    },
+  });
   return (
-    <div className="p-5">
+    <div className="py-5">
       <Image
         src="/banner-home-1.png"
         height={0}
         width={0}
         alt="Até 55% de desconto esse mes"
-        className="h-auto w-full"
+        className="h-auto w-full px-5"
         sizes="100vw"
       />
 
-      <div className="mt-8">
+      <div className="mt-8 px-5">
         <Categories />
+      </div>
+
+      <div className="mt-8">
+        <ProductList products={deals}/>
       </div>
     </div>
   );
