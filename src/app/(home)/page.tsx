507bd652b02fa8697e7a667c493eb26ee5600eb6
Jhony Pereira <jhony-16@live.com>
2023-10-17 22:02:46 -0300

    FEAT: add first dicount banner to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,18 @@
 "use client";
 
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
-  return <div>
-    
-  </div>;
+  return (
+    <div>
+      <Image
+        src="/banner-home-1.png"
+        height={0}
+        width={0}
+        alt="Até 55% de desconto esse mes"
+        className="h-auto w-full"
+        sizes="100vw"
+      />
+    </div>
+  );
 }
