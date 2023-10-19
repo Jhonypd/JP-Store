9f9eabb79ebe74494c91841245b142a00d46b665
Jhony Pereira <jhony-16@live.com>
2023-10-19 15:33:00 -0300

    feat: added ProductInfo components

---
+++ src/app/product/[slug]/components/product-info.tsx
@@ -0,0 +1,90 @@
+"use client"
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { ProductWithTotalPrice } from "@/helpers/products";
+import { ArrowDownIcon, ArrowLeft, ArrowRight, TruckIcon } from "lucide-react";
+import { useState } from "react";
+
+interface ProductInfoProps {
+    product: Pick<
+        ProductWithTotalPrice,
+        "basePrice"
+        | "description"
+        | "discountPercentage"
+        | "totalPrice"
+        | 'name'
+    >
+}
+
+const ProductInfo = ({ product: { name, basePrice, description, discountPercentage, totalPrice } }: ProductInfoProps) => {
+
+    const [quantity, setQuantity] = useState(1)
+
+    const handleDecreaseQuantityClick = () => {
+        setQuantity((prev) => (prev === 1 ? prev : prev - 1))
+    }
+    const handleIncreaseQuantityClick = () => {
+        setQuantity((prev) => prev + 1)
+    }
+
+    return (
+        <div className="flex flex-col px-5">
+
+            <h2 className="text-lg">{name}</h2>
+
+            <div className="flex items-center gap-2">
+
+                <h1 className="text-xl font-bold">R$ {totalPrice.toFixed(2)}</h1>
+
+                {discountPercentage > 0 && (
+                    <Badge className="bg-secondary px-2 py-[2px]">
+                        <ArrowDownIcon size={14} />{discountPercentage}%
+                    </Badge>
+                )}
+
+            </div>
+            {discountPercentage > 0 && (
+                <p className="opacity-75 text-sm line-through">
+                    R$ {Number(basePrice).toFixed(2)}
+                </p>
+            )}
+            <div className="flex items-center gap-2 mt-4">
+                <Button size={'icon'} variant={'outline'} onClick={handleDecreaseQuantityClick}>
+                    <ArrowLeft />
+                </Button>
+
+                <span>{quantity}</span>
+
+                <Button size={'icon'} variant={'outline'} onClick={handleIncreaseQuantityClick}>
+                    <ArrowRight />
+                </Button>
+            </div>
+
+            <div className="flex flex-col gap-3">
+                <h3 className="font-bold">Descrição</h3>
+                <p className="text-sm opacity-60 text-justify">{description}</p>
+            </div>
+
+            <Button className="mt-8 uppercase bg-secondary font-bold">
+                Adicionar ao carrinho
+            </Button>
+
+            <div className="rounded-lg bg-accent flex items-center justify-between px-5 py-2 mt-5">
+                <div className="flex items-center gap-3">
+                    <TruckIcon />
+                    <div className="flex flex-col">
+                        <p className="text-xs">
+                            Entrega via <span className="font-bold">JPpacket &reg;</span>
+                        </p>
+                        <p className="text-xs text-secondary">
+                            Envio para <span className="font-bold">todo Brasil</span>
+                        </p>
+                    </div>
+                </div>
+                <p className="text-xs font-bold">Frete grátis</p>
+            </div>
+        </div>
+    );
+}
+
+export default ProductInfo;
--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,12 +1,14 @@
 import { prismaClient } from "@/lib/prisma";
 import ProductImages from "./components/product-images";
+import ProductInfo from "./components/product-info";
+import { computeProductTotalPrice } from "@/helpers/products";
 
 interface ProductDetailsPageProps {
     params: {
         slug: string
     }
 }
-const ProductDetailsPage = async ({ params: { slug } }: { params: { slug: string } }) => {
+const ProductDetailsPage = async ({ params: { slug }, }: ProductDetailsPageProps) => {
     const product = await prismaClient.product.findFirst({
         where: {
             slug: slug
@@ -14,8 +16,9 @@ const ProductDetailsPage = async ({ params: { slug } }: { params: { slug: string
     })
     if (!product) return null
     return (
-        <div>
+        <div className="flex flex-col gap-8">
             <ProductImages name={product.name} imagesUrls={product.imageUrls} />
+            <ProductInfo product={computeProductTotalPrice(product)} />
         </div>
 
     );
