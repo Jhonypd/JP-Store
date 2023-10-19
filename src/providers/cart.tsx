024c4e32c5f6431cb2599d49b1d80167c0c5a477
Jhony Pereira <jhony-16@live.com>
2023-10-19 17:04:03 -0300

    feat: added CartContext

---
+++ src/providers/cart.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import { Product } from "@prisma/client"
+import { ReactNode, createContext } from "react";
+
+interface CartProduct extends Product {
+    quantity: number
+}
+
+interface IcartContext {
+    products: CartProduct[];
+    cartTotalPrice: number;
+    cartBasePrice: number;
+    cartTotalDicount: number;
+}
+
+const CartContext = createContext<IcartContext>({
+    products: [],
+    cartTotalPrice: 0,
+    cartBasePrice: 0,
+    cartTotalDicount: 0
+})
+const CartProvider = ({ children }: { children: ReactNode }) => {
+    return (
+        <CartContext.Provider value={{
+            products: [],
+            cartTotalPrice: 0,
+            cartBasePrice: 0,
+            cartTotalDicount: 0
+        }}>
+            {children}
+        </CartContext.Provider>
+    );
+}
+
+export default CartProvider;
