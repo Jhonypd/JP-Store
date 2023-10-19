94f2fefb94d619dc8ccf84130e543ec621d44e85
Jhony Pereira <jhony-16@live.com>
2023-10-19 14:07:20 -0300

    feat: added ProductImages components

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,10 +1,24 @@
+import { prismaClient } from "@/lib/prisma";
+import ProductImages from "./components/product-images";
+
 interface ProductDetailsPageProps {
-    params:{
+    params: {
         slug: string
     }
 }
-const ProductDetailsPage = async ({params:{slug}}: {params: {slug:string}}) => {
-    return (  );
+const ProductDetailsPage = async ({ params: { slug } }: { params: { slug: string } }) => {
+    const product = await prismaClient.product.findFirst({
+        where: {
+            slug: slug
+        }
+    })
+    if (!product) return null
+    return (
+        <div>
+            <ProductImages name={product.name} imagesUrls={product.imageUrls} />
+        </div>
+
+    );
 }
- 
+
 export default ProductDetailsPage;
