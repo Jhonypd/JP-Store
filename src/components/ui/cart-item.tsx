6bf9b5afa2d1cd55048a3f594e9763ba03ba7e38
Jhony Pereira <jhony-16@live.com>
2023-10-19 22:35:51 -0300

    feat: ensure product quantity can be modified on cart

--- src/components/ui/cart-item.tsx
+++ src/components/ui/cart-item.tsx
@@ -1,53 +1,69 @@
-import { CartProduct } from "@/providers/cart";
+import { CartContext, CartProduct } from "@/providers/cart";
 import { ArrowLeft, ArrowRight, TrashIcon } from "lucide-react";
-import Image from 'next/image'
+import Image from "next/image";
 import { Button } from "./button";
+import { useContext } from "react";
 
 interface CartItemProps {
-    product: CartProduct
+  product: CartProduct;
 }
 
 const CartItem = ({ product }: CartItemProps) => {
-    return (
-        <div className="flex items-center justify-between">
-            <div className="flex items-center gap-4">
-                <div className="bg-accent flex items-center justify-center rounded-ld h-[77px] w-[77px]">
-                    <Image
-                        src={product.imageUrls[0]}
-                        alt={product.name}
-                        width={0}
-                        height={0}
-                        sizes="100vw"
-                        className="h-auto max-h-[70] w-auto max-w-[80%]"
-                    />
-                </div>
-                <div className="flex flex-col">
-                    <p className="text-xs">{product.name}</p>
-                    <div className="flex items-center gap-2">
-                        <p className="font-bold text-sm">R$ {product.totalPrice.toFixed(2)}</p>
-                        {product.discountPercentage > 0 && (
-                            <p className="opacity-75 line-through text-xs">R$ {Number(product.basePrice).toFixed(2)}</p>
-                        )}
-                    </div>
-                    <div className="flex items-center gap-1">
-                        <Button size={'icon'} variant={'outline'} className="h-8 w-8" >
-                            <ArrowLeft />
-                        </Button>
+  const { decreaseProductQuantity } = useContext(CartContext);
 
-                        <span className="text-xs">{product.quantity}</span>
+  const handleDecreaseProductQuantityClick = () => {
+    decreaseProductQuantity(product.id);
+  };
 
-                        <Button size={'icon'} variant={'outline'} className="h-8 w-8" >
-                            <ArrowRight />
-                        </Button>
-                    </div>
-                </div>
-            </div>
-
-            <Button variant={'outline'} size={'icon'}>
-                <TrashIcon size={16} />
-            </Button>
+  return (
+    <div className="flex items-center justify-between">
+      <div className="flex items-center gap-4">
+        <div className="rounded-ld flex h-[77px] w-[77px] items-center justify-center bg-accent">
+          <Image
+            src={product.imageUrls[0]}
+            alt={product.name}
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="h-auto max-h-[70] w-auto max-w-[80%]"
+          />
         </div>
-    );
-}
+        <div className="flex flex-col">
+          <p className="text-xs">{product.name}</p>
+          <div className="flex items-center gap-2">
+            <p className="text-sm font-bold">
+              R$ {product.totalPrice.toFixed(2)}
+            </p>
+            {product.discountPercentage > 0 && (
+              <p className="text-xs line-through opacity-75">
+                R$ {Number(product.basePrice).toFixed(2)}
+              </p>
+            )}
+          </div>
+          <div className="flex items-center gap-1">
+            <Button
+              size={"icon"}
+              variant={"outline"}
+              className="h-8 w-8"
+              onClick={handleDecreaseProductQuantityClick}
+            >
+              <ArrowLeft size={16} />
+            </Button>
+
+            <span className="text-xs">{product.quantity}</span>
+
+            <Button size={"icon"} variant={"outline"} className="h-8 w-8">
+              <ArrowRight size={16} />
+            </Button>
+          </div>
+        </div>
+      </div>
+
+      <Button variant={"outline"} size={"icon"}>
+        <TrashIcon size={16} />
+      </Button>
+    </div>
+  );
+};
 
 export default CartItem;
--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -1,4 +1,4 @@
-import { ShapesIcon, ShoppingBagIcon, ShoppingCartIcon } from "lucide-react";
+import { ShoppingCartIcon } from "lucide-react";
 import { Badge } from "./badge";
 import { useContext } from "react";
 import { CartContext } from "@/providers/cart";
@@ -6,22 +6,27 @@ import CartItem from "./cart-item";
 import { computeProductTotalPrice } from "@/helpers/products";
 
 const Cart = () => {
+  const { products } = useContext(CartContext);
 
-    const { products } = useContext(CartContext)
-
-    return (
-        <div className="flex flex-col gap-8">
-            <Badge className="w-fit px-3 py-[0.375rem] gap-1 text-base uppercase border-primary" variant='outline'>
-                <ShoppingCartIcon size={16} />
-                catálogo
-            </Badge>
-            <div className="flex flex-col gap-5">
-                {products.map((product) => (
-                    <CartItem key={product.id} product={computeProductTotalPrice(product as any) as any} />
-                ))}
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="flex flex-col gap-8">
+      <Badge
+        className="w-fit gap-1 border-primary px-3 py-[0.375rem] text-base uppercase"
+        variant="outline"
+      >
+        <ShoppingCartIcon size={16} />
+        catálogo
+      </Badge>
+      <div className="flex flex-col gap-5">
+        {products.map((product) => (
+          <CartItem
+            key={product.id}
+            product={computeProductTotalPrice(product as any) as any}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Cart;
