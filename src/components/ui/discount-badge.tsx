fbea7901b7113be4426205666a006a92c1cf1730
Jhony Pereira <jhony-16@live.com>
2023-10-20 16:44:49 -0300

    style: modified the mode show information totalbase the cart, and style the hover the button.

--- src/components/ui/discount-badge.tsx
+++ src/components/ui/discount-badge.tsx
@@ -3,11 +3,18 @@ import { Badge, BadgeProps } from "./badge";
 import { twMerge } from "tailwind-merge";
 
 const DiscountBadge = ({ children, className, ...props }: BadgeProps) => {
-    return (
-        <Badge {...props} className={twMerge("bg-secondary px-2 py-[2px]", className)}>
-            <ArrowDownIcon size={14} />{children}%
-        </Badge>
-    );
-}
+  return (
+    <Badge
+      {...props}
+      className={twMerge(
+        "bg-secondary px-2 py-[2px] hover:bg-secondary",
+        className,
+      )}
+    >
+      <ArrowDownIcon size={14} />
+      {children}%
+    </Badge>
+  );
+};
 
 export default DiscountBadge;
