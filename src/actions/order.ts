20a2f260ab015afd71d53f03de5c6308a1eded6d
Jhony Pereira <jhony-16@live.com>
2023-10-24 14:28:09 -0300

    feat: add creatOrder server action

---
+++ src/actions/order.ts
@@ -0,0 +1,27 @@
+"use server";
+import { prismaClient } from "@/lib/prisma";
+import { CartProduct } from "@/providers/cart";
+
+export const createOrder = async (
+  cartProducts: CartProduct[],
+  userId: string,
+) => {
+  const order = await prismaClient.order.create({
+    data: {
+      userId,
+      status: "WAITTING_FOR_PAYMENT",
+      orderProduct: {
+        createMany: {
+          data: cartProducts.map((product) => ({
+            basePrice: product.basePrice,
+            discountPercentage: product.discountPercentage,
+            productId: product.id,
+            quantity: product.quantity,
+          })),
+        },
+      },
+    },
+  });
+
+  return order;
+};
