fdc5a72c1e58a9867d65f7093252a032d44867a6
Jhony Pereira <jhony-16@live.com>
2024-02-20 09:48:17 -0300

    fix: modfieded nextConfig

---
+++ next.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    // domains: ["fsw-store.s3.sa-east-1.amazonaws.com"],
+    remotePatterns: [
+      {
+        hostname: "fsw-store.s3.sa-east-1.amazonaws.com",
+      },
+    ],
+  },
+  experimental: {
+    serverActions: true,
+  },
+};
+
+export default nextConfig;
